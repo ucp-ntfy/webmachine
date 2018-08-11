4380262c61aa0fd0e29431f80358f27349a9a8d4
Martin Cox <me@mar.tin.cx>
2018-08-11 14:54:57 +0100

    Remove get_stacktrace/0 call.

--- src/webmachine_decision_core.erl
+++ src/webmachine_decision_core.erl
@@ -32,8 +32,8 @@ handle_request(Resource, ReqState) ->
     try
         d(v3b13)
     catch
-        error:_ ->
-            error_response(erlang:get_stacktrace())
+        error:_:S ->
+            error_response(S)
     end.
 
 wrcall(X) ->
